531be7ddb8cdd56fcfd2349816b68a6151715790
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-27 06:01:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,10 @@
 
 def reverse_each_word(string)
   array_string = string.split(" ")
+  reverse_string = []
   array_string.collect do |word|
     word.reverse
+    reverse_string << word.reverse
   end
-  array_string.join(" ")
+  reverse_string.join(" ")
 end
